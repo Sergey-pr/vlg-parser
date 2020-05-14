383ed146384f77b55508832332c5b5fe0c8a2121
Sergey Prohorenko <s.prohorenko@brandquad.ru>
2020-05-14 21:55:12 +0400

    changes wsgi.py

--- vlg_parser_django/vlg_parser_django/wsgi.py
+++ vlg_parser_django/vlg_parser_django/wsgi.py
@@ -6,8 +6,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/root/workspace/vlg_parser/source/vlg_parser_django')
+sys.path.append('/root/workspace/vlg_parser/.py_env/lib/python3.7/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vlg_parser_django.settings')
 
