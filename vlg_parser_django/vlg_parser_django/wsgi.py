7e9fc4c585d6d1e47422202e0804697c7c7b808d
Sergey Prohorenko <s.prohorenko@brandquad.ru>
2020-05-14 21:12:05 +0400

    revert to default wsgi.py

--- vlg_parser_django/vlg_parser_django/wsgi.py
+++ vlg_parser_django/vlg_parser_django/wsgi.py
@@ -1,22 +1,14 @@
+"""
+WSGI config for vlg_parser_django project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
 import os
-import time
-import traceback
-import signal
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/root/workspace/vlg_parser/source/vlg_parser_django')
-# adjust the Python version in the line below as needed
-sys.path.append('/root/workspace/vlg_parser/.py_env/lib/python3.7/site-packages')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vlg_parser_django.settings')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vlg_parser.settings")
-
-try:
-    application = get_wsgi_application()
-except Exception:
-    # Error loading applications
-    if 'mod_wsgi' in sys.modules:
-        traceback.print_exc()
-        os.kill(os.getpid(), signal.SIGINT)
-        time.sleep(2.5)
+application = get_wsgi_application()
